90d0666fd4ef43dcd631560980ef0d8497a64b49
Zeeshan Ali <zba05392@uga.edu>
2022-04-21 02:20:14 -0400

    Added Firebase and Sign in

--- app/src/main/java/edu/uga/cs/shareshop/MainActivity.java
+++ app/src/main/java/edu/uga/cs/shareshop/MainActivity.java
@@ -1,14 +1,89 @@
 package edu.uga.cs.shareshop;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signInButton;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInButton = findViewById(R.id.button);
+        registerButton = findViewById(R.id.button2);
+
+        signInButton.setOnClickListener(new SignInButtonOnClickListener());
+
     }
+
+    private class SignInButtonOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    //.setTheme(R.style.LoginTheme)
+                    .build();
+            signInLauncher.launch(signInIntent);
+        }
+    }
+
+    // The ActivityResultLauncher class provides a new way to invoke an activity
+    // for some result.  It is a replacement for the deprecated method startActivityForResult.
+    //
+    // The signInLauncher variable is a launcher to start the AuthUI's logging in process that
+    // should return to the MainActivity when completed.  The overridden onActivityResult
+    // is then called when the Firebase logging-in process is finished.
+    private ActivityResultLauncher<Intent> signInLauncher =
+            registerForActivityResult(
+                    new FirebaseAuthUIActivityResultContract(),
+                    new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                        @Override
+                        public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                            onSignInResult(result);
+                        }
+                    }
+            );
+
+    // This method is called (above) once the Firebase sign-in activity returns (completes).
+    // The current (logged-in) Firebase user can be obtained.
+    // Then, there is a transition to the JobLeadManagementActivity.
+    private void onSignInResult( FirebaseAuthUIAuthenticationResult result ) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            // after a successful sign in, start the job leads management activity
+            Intent intent = new Intent( this, MainMenu.class );
+            startActivity( intent );
+        }
+        else {
+            // Sign in failed. If response is null the user canceled the
+            Toast.makeText( getApplicationContext(),
+                    "Sign in failed",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
